29392927ca45bcc7efa3884bde7e49f9140bf601
ZeydCP <zchamsipasha@gmail.com>
2023-11-20 06:03:59 +0000

    testing pre processor

--- part1_nn_lib.py
+++ part1_nn_lib.py
@@ -673,8 +673,8 @@ class Preprocessor(object):
         #######################################################################
 
         # Compute mean and standard deviation for each feature
-        self.mean_values = np.mean(data, axis=0)
-        self.std_values = np.std(data, axis=0)
+        self.mean_values = np.mean(data)
+        self.std_values = np.std(data)
 
         #######################################################################
         #                       ** END OF YOUR CODE **
